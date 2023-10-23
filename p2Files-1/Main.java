332b34a693a6a0064539c01d477086227b55cc4c
joelathrop <jhlathrop24@gmail.com>
2023-10-23 10:45:36 -0600

    Update Main.java

--- p2Files-1/Main.java
+++ p2Files-1/Main.java
@@ -1,6 +1,26 @@
+import fa.nfa.NFA;
+
+import java.util.Set;
+
 public class Main {
 
     public static void Main(String[] args) {
+        NFA nfa = new NFA();
 
+        nfa.addSigma('0');
+        nfa.addSigma('1');
+
+        nfa.addState("a");
+        nfa.setStart("a");
+        nfa.addState("b");
+        nfa.setFinal("b");
+
+        nfa.addTransition("a", Set.of("a"), '0');
+        nfa.addTransition("a", Set.of("b"), '1');
+        nfa.addTransition("b", Set.of("a"), 'e');
+
+        int ret = nfa.maxCopies("1");
+
+        System.out.println(ret);
     }
 }
