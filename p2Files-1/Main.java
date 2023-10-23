ccb6f279e151264335f5238817e784d0581a1238
joelathrop <jhlathrop24@gmail.com>
2023-10-23 10:46:25 -0600

    Update Main.java

--- p2Files-1/Main.java
+++ p2Files-1/Main.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 public class Main {
 
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         NFA nfa = new NFA();
 
         nfa.addSigma('0');
